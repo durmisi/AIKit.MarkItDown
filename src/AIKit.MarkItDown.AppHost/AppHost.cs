ddb9d677ad43f21d07e1a37660fcd14235a6400a
Admir Durmishi <durmisi@gmail.com>
2026-02-05 11:30:47 +0100

    Refactor project structure: rename test project and remove unused test files

--- src/AIKit.MarkItDown.AppHost/AppHost.cs
+++ src/AIKit.MarkItDown.AppHost/AppHost.cs
@@ -1,11 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AIKit_MarkItDown_Test>("aikit-markitdown-test");
-
-
-var fastApiApp = builder.AddUvicornApp("MarkItDown", "./AIKit.MarkItDown.Api", "main:app")
-    .WithUv()
-    .WithExternalHttpEndpoints()
+var fastApiApp = builder.AddUvicornApp("MarkItDown", "../AIKit.MarkItDown.Api", "main:app")
+    .WithHttpEndpoint(name: "api", port: 8000, env: "PORT")
     .WithHttpHealthCheck("/health");
 
 
